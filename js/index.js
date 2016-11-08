948216e2585abaa856188282984f69e1b3f6336e
Jack Phelan <jackphelan461@gmail.com>
2016-11-07 22:44:34 -0800

    index line 71: remove done button

--- js/index.js
+++ js/index.js
@@ -1,8 +1,45 @@
 function unhideMail() {
 		document.getElementById('email-form').style = "display:block;";
-		document.getElementById('sms-form').style = "display:none;";
+		document.getElementById('sms-form').style = "display:none;";		
 }
 function unhideSMS() {
 		document.getElementById('sms-form').style = "display:block;";
 		document.getElementById('email-form').style = "display:none;";
+}
+
+
+
+function checkForm() {
+	if (emailChecked()) {
+	if (document.getElementById('email-text').value == "") {
+		Materialize.toast('No email input', 2000);
+	}
+	}
+	if (smsChecked()) {
+		if (document.getElementById('email-text').value == "") {
+		Materialize.toast('No phone input', 2000);
+	}
+	}
+}
+
+function emailChecked () {
+	var inputs = document.getElementById('test1').checked;
+	if (inputs == true) {
+		return true;
+	}
+	else if (inputs == false) {
+		return false;
+	}
+	 
+}
+
+function smsChecked () {
+	var inputs = document.getElementById('test2').checked;
+	if (inputs == true) {
+		return true;
+	}
+	else if (inputs == false) {
+		return false;
+	}
+	 
 }
